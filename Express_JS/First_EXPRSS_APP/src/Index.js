5d753b2621c425ac2d0878a8b00f2a15065163ea
manish-dev3490 <kmanish87064@gmail.com>
2024-07-11 19:11:52 +0530

    interacting with postman

--- Express_JS/First_EXPRSS_APP/src/Index.js
+++ Express_JS/First_EXPRSS_APP/src/Index.js
@@ -1,27 +1,34 @@
-const express = require('express');
+const express = require("express");
+const bodyparser = require("body-parser");
 
 // This is our port number
 const PORT = 3000;
 
 // Our Server is created behind the scene it is using http.createServer((req, res)=>{}) for http and tcp it is using net.createServer(()=>{})
 const APP = express();
+APP.use(bodyparser.json());
 
-// Initializinf middlewares 
+// Initializinf middlewares
 function m1(req, res, next) {
-    console.log("Inside middleware 1");
-    next();
+  console.log("Inside middleware 1");
+  next();
 }
 
-function m2(req, res) {
-    console.log("Inside middleware 2");
-    next();
+function m2(req, res, next) {
+  console.log("Inside middleware 2");
+  next();
 }
 
-APP.get('/home', m1,m2, (req, res) => {
-    console.log("Api has treturned the response");
-    res.send("hello world");
+APP.get("/home", m1, m2, (req, res) => {
+  console.log("Api has treturned the response");
+  res.send("hello world");
+});
+
+APP.post("/product", (req, res) => {
+  res.send("post request created");
+  console.log(req.body);
 });
 
 APP.listen(PORT, () => {
-    console.log("Server is listening on port 3000");
+  console.log("Server is listening on port 3000");
 });
