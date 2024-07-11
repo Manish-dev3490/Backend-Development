a4dce7b967c29e41fe7aadd930ce1f5acfa311c2
manish-dev3490 <kmanish87064@gmail.com>
2024-07-11 15:02:09 +0530

    USing middlewares

--- Express_JS/First_EXPRSS_APP/src/Index.js
+++ Express_JS/First_EXPRSS_APP/src/Index.js
@@ -6,7 +6,19 @@ const PORT = 3000;
 // Our Server is created behind the scene it is using http.createServer((req, res)=>{}) for http and tcp it is using net.createServer(()=>{})
 const APP = express();
 
-APP.get('/home', (req, res) => {
+// Initializinf middlewares 
+function m1(req, res, next) {
+    console.log("Inside middleware 1");
+    next();
+}
+
+function m2(req, res, next) {
+    console.log("Inside middleware 2");
+    next();
+}
+
+APP.get('/home', m2,m1, (req, res) => {
+    console.log("Api has treturned the response");
     res.send("hello world");
 });
 
