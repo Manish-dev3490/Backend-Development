83ccba06da01cb640bcd59a648ee56da1b1371b3
manish-dev3490 <kmanish87064@gmail.com>
2024-07-21 15:08:03 +0530

    created routes

--- Express_JS/Practice_Server/src/index.js
+++ Express_JS/Practice_Server/src/index.js
@@ -1,12 +1,10 @@
 // in this file i am goiung to make a server
 const express=require('express');
 const APirouter = require('./routes/apirouter');
-const Productsrouter = require('./routes/ProductsRoutes');
 const app=express();
 const PORT=3000;
 
-app.use(APirouter);
-app.use(Productsrouter)
+app.use('/api',APirouter);
 
 app.listen(PORT,()=>{
     console.log("Our server is started at port",PORT);
