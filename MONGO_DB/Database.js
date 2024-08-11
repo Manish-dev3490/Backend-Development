aa5594356f159bf7d0142c9f5e85ab1b29b2475b
manish-dev3490 <kmanish87064@gmail.com>
2024-08-11 19:30:00 +0530

    writing summary for all codes for understanding

--- MONGO_DB/Database.js
+++ MONGO_DB/Database.js
@@ -1,23 +1,30 @@
-const {MongoClient}=require('mongodb');
+const { MongoClient } = require('mongodb');
 
-const URL="mongodb+srv://kmanish87064:GHFbZnpzWKfHzTj9@setup1.byop8.mongodb.net/";
-const client=new MongoClient (URL);
+const URL = "mongodb+srv://kmanish87064:GHFbZnpzWKfHzTj9@setup1.byop8.mongodb.net/";
+const client = new MongoClient(URL);
 
-const dbName="University-Data";
+const dbName = "University-Data";
 
-async function main(){
+async function main() {
     // It will coneect the client with the mongo db serever hosted at AWS 
-    await client.connect(); 
+    await client.connect();
+    // it will print after client get to connected with the mongodb server;
     console.log('Connected successfully to server');
-    const db=client.db(dbName);
-    const collection=db.collection('user');
-    
+
+    // we are are reffering to the Collection which we have created in compass using GUI interface 
+    const db = client.db(dbName);
+    // we are are reffering to the document named user which we have created in compass using GUI interface 
+    const collection = db.collection('user');
+
+    // we are getting access to al the documents presented in our document
+    const findResult = await collection.find({}).toArray();
+    console.log('Found documents =>', findResult);
 
     return 'Done.';
 }
 
-const connectionDone=main();
+const connectionDone = main();
 connectionDone
-.then(console.log)
-.catch(console.error)
-.finally(()=>client.close());
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
