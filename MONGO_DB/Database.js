90fb7daefe399fbbb9e12b820815a538ad3c4b80
manish-dev3490 <kmanish87064@gmail.com>
2024-08-11 19:21:04 +0530

    Connected with the mongodb serverv at aws

--- MONGO_DB/Database.js
+++ MONGO_DB/Database.js
@@ -1,1 +1,23 @@
-const URI="mongodb+srv://kmanish87064:<GHFbZnpzWKfHzTj9>@setup1.byop8.mongodb.net/"
+const {MongoClient}=require('mongodb');
+
+const URL="mongodb+srv://kmanish87064:GHFbZnpzWKfHzTj9@setup1.byop8.mongodb.net/";
+const client=new MongoClient (URL);
+
+const dbName="University-Data";
+
+async function main(){
+    // It will coneect the client with the mongo db serever hosted at AWS 
+    await client.connect(); 
+    console.log('Connected successfully to server');
+    const db=client.db(dbName);
+    const collection=db.collection('user');
+    
+
+    return 'Done.';
+}
+
+const connectionDone=main();
+connectionDone
+.then(console.log)
+.catch(console.error)
+.finally(()=>client.close());
