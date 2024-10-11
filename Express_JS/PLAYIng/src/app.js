392f2d0f69aaaaa8e859e9c44a91696ac1358de9
manish-dev3490 <kmanish87064@gmail.com>
2024-10-11 16:55:55 +0530

    playing with routes

--- Express_JS/PLAYIng/src/app.js
+++ Express_JS/PLAYIng/src/app.js
@@ -1,10 +1,18 @@
 const express=require("express");
 const app=express();
 
+app.use("/hello",(req,res)=>{
+    res.send("hello ehllo helllo");
+})
 
+app.use("/hello/v1",(req,res)=>{
+    res.send("hello ehllo helllo v2 vaala hooo");
+})
 app.use("/",(req,res)=>{
     res.send("i am coming from server");
 })
+
+
 
 app.listen(3000,()=>{
     console.log("i am listening on 3000");
